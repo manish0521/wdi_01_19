08f949eac2114c6fbeea0f4f8ad072d50732f1a4
Manish Singh <manish.singh@codeimmersives.com>
2019-06-05 14:21:39 -0400

    date added

--- projects/ajax/stock exchange/script.js
+++ projects/ajax/stock exchange/script.js
@@ -4,6 +4,9 @@ window.onload = function () {
 
 function init() { 
     document.getElementById("submit").addEventListener("click", submit); 
+    document.getElementById("lead").innerHTML = formatAMPM();
+
+
     let httpRequest 
 
     function makeRequest() {
@@ -77,7 +80,18 @@ function init() {
         makeRequest()
 
     }
+
+    function formatAMPM() {
+        let d = new Date(),
+            minutes = d.getMinutes().toString().length == 1 ? '0'+d.getMinutes() : d.getMinutes(),
+            hours = d.getHours().toString().length == 1 ? '0'+d.getHours() : d.getHours(),
+            ampm = d.getHours() >= 12 ? 'pm' : 'am',
+            months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'],
+            days = ['Sun','Mon','Tue','Wed','Thu','Fri','Sat'];
+        return days[d.getDay()]+' '+months[d.getMonth()]+' '+d.getDate()+' '+d.getFullYear()+' '+hours+':'+minutes+ampm;
+        }
     
 
-    makeRequest()
+    makeRequest();
+    formatAMPM();
 }
